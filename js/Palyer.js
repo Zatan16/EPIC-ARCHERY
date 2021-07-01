8f08ca2bf995a02b8d12a571718c69b78e73febb
Zatan16 <79738479+Zatan16@users.noreply.github.com>
2021-07-01 12:31:28 +0530

    Update Palyer.js

--- js/Palyer.js
+++ js/Palyer.js
@@ -21,7 +21,7 @@ class Player {
         rotate(angle);
         imageMode(CENTER);
 
-        image(this.image, 0, 0, this.w, this.h);
+        image(this.img, 0, 0, this.w, this.h);
         pop();
     }
 }
